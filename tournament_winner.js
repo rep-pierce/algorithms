a43e989ac2c177cded13dc1ee62bf5a49d143b48
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-02-27 10:20:16 -0700

    update

--- tournament_winner.js
+++ tournament_winner.js
@@ -11,4 +11,26 @@ function tournamentWinner(competitions, results) {
     return Object.keys(points).reduce((a, b) => points[a] > points[b] ? a : b);
 }
 
+function tournamentWinnerOther(competitions, results) {
+    const leader = {score: -Infinity, name: ''}
+    const scoreboard = {}
+  
+    for (let i = 0; i < competitions.length; i++){
+      const winnerIdx = results[i] === 0 ? 1 : 0
+      const winner = competitions[i][winnerIdx]
+  
+      if (winner in scoreboard) scoreboard[winner] += 3
+      else scoreboard[winner] = 3
+  
+      if (leader.score < scoreboard[winner]) {
+        leader.name = winner
+        leader.score = scoreboard[winner]
+      }
+    }
+    return leader.name
+  }
+  
+  // Do not edit the line below.
+  exports.tournamentWinner = tournamentWinner;
+
 console.log(tournamentWinner([["ryan", "ashley"], ["adam", "jaxon"], ["ryan", "adam"], ["ryan", "jaxon"], ["ashley", "adam"], ["ashley", "jaxon"]], [1, 0, 0, 1, 0, 1]))
