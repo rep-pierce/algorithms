217b9b544aa3cef369c19953a4f48b639798d052
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-10 15:58:35 -0700

    update

---
+++ calculate_average.js
@@ -0,0 +1,11 @@
+function findAverage(array) {
+    let count = 0
+    if (array == ''){
+      return 0;
+    }else {
+      for (number of array){
+        count = number + count ;
+      }return count/array.length
+    }
+  
+  }
