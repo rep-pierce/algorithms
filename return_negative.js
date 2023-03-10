e3640a2dc052a32f3a1fdab5d0ad1062c8b7cf75
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-10 16:01:31 -0700

    update

---
+++ return_negative.js
@@ -0,0 +1,15 @@
+// function makeNegative(num) {
+//     return num > 0 ? num*-1 : num
+//   }
+
+// function makeNegative(num) {
+//     return num = num > 0? num*-1 : num
+//   }
+
+function makeNegative(num) {
+    if (num > 0){
+      return num*-1
+    }else{
+      return num
+    }
+  }
