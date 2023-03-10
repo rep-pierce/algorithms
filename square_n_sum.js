b99ebc90b01641ab59e4a00b5643e76b5ef5045c
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-10 16:03:40 -0700

    update

--- square_n_sum.js
+++ square_n_sum.js
@@ -1,7 +1,25 @@
-function squareSum(numbers){
-    let sum = 0
-    for (number of numbers){
-      sum += number**2
+// function squareSum(numbers){
+//     let sum = 0
+//     for (number of numbers){
+//       sum += number**2
+//     }
+//     return sum
+//   }
+
+//   function squareSum(numbers){
+//     let sumOfNumbers = 0
+//     for (number of numbers){
+//       let n = number**2
+//       sumOfNumbers = sumOfNumbers + n ;
+//     }
+//     return sumOfNumbers;
+//   }
+
+  function squareSum(numbers){
+    const sumsOfNumbers = [];
+    for (number of numbers){    
+      let n = number**2;
+      sumsOfNumbers.push(n);
     }
-    return sum
+    return sumsOfNumbers.reduce((partialSum, a) => partialSum + a, 0);
   }
