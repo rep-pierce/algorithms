346b1f5d50603389b7c144b004cf0e3f4bb865b5
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-03-10 15:59:14 -0700

    update

---
+++ grasshopper_personalized_message.js
@@ -0,0 +1,8 @@
+// function greet (name, owner) {
+//     return name === owner? 'Hello boss' : 'Hello guest'
+//   }
+
+function greet (name, owner) {
+    const greeting = name == owner ? 'Hello boss' : 'Hello guest'
+    return greeting
+  }
