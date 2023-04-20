5f1b1923fb8fcc01c655992b788e70885640b657
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-04-20 14:27:39 -0600

    small difference better

--- smallest_difference.py
+++ smallest_difference.py
@@ -1,15 +1,43 @@
+# def smallestDifference(array1, array2):
+#     smallest_difference = None
+#     small_array = []
+
+#     for i in array1:
+#         for j in array2:
+#             difference = abs(i-j)
+#             if smallest_difference == None:
+#                 smallest_difference = difference
+#                 small_array = [i, j]
+#             elif difference < smallest_difference:
+#                 smallest_difference = difference
+#                 small_array = [i, j]
+#     return small_array
+
 def smallestDifference(array1, array2):
-    smallest_difference = None
-    small_array = []
+    array1.sort()
+    array2.sort()
 
-    for i in array1:
-        for j in array2:
-            difference = abs(i-j)
-            if smallest_difference == None:
-                smallest_difference = difference
-                small_array = [i, j]
-            elif difference < smallest_difference:
-                smallest_difference = difference
-                small_array = [i, j]
-    return small_array
+    x = len(array1) - 1
+    y = len(array2) - 1
 
+    small_diff = float('inf')
+    small_pair = []
+
+    while x >= 0 and y >= 0:
+        diff = abs(array1[x] - array2[y])
+        if diff == 0:
+            return [array1[x], array2[y]]
+        elif diff < small_diff:
+            small_diff = diff
+            small_pair = [array1[x], array2[y]]
+
+        if array1[x] < array2[y]:
+            y -= 1
+        else:
+            x -= 1
+
+    return small_pair
+
+
+        
+    
