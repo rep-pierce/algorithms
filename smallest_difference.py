57342fc4a9da397275c9f351b353efe621c0370c
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-04-20 14:03:58 -0600

    small difference

---
+++ smallest_difference.py
@@ -0,0 +1,14 @@
+def smallestDifference(array1, array2):
+    smallest_difference = None
+    small_array = []
+
+    for i in array1:
+        for j in array2:
+            difference = abs(i-j)
+            if smallest_difference == None:
+                smallest_difference = difference
+                small_array = [i, j]
+            elif difference < smallest_difference:
+                smallest_difference = difference
+                small_array = [i, j]
+    return small_array
