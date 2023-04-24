9ecb2600b0f07931cba6180053cc8e9ead7b9356
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-04-24 10:34:25 -0600

    update

--- smallest_difference.py
+++ smallest_difference.py
@@ -13,30 +13,60 @@
 #                 small_array = [i, j]
 #     return small_array
 
-def smallestDifference(array1, array2):
-    array1.sort()
-    array2.sort()
+# Time = O(Nlog(N) + MLog(M))
+# def smallestDifference(array1, array2):
+#     array1.sort()
+#     array2.sort()
 
-    x = len(array1) - 1
-    y = len(array2) - 1
+#     x = len(array1) - 1
+#     y = len(array2) - 1
 
-    small_diff = float('inf')
-    small_pair = []
+#     small_diff = float('inf')
+#     small_pair = []
 
-    while x >= 0 and y >= 0:
-        diff = abs(array1[x] - array2[y])
-        if diff == 0:
-            return [array1[x], array2[y]]
-        elif diff < small_diff:
-            small_diff = diff
-            small_pair = [array1[x], array2[y]]
+#     while x >= 0 and y >= 0:
+#         diff = abs(array1[x] - array2[y])
+#         if diff == 0:
+#             return [array1[x], array2[y]]
+#         elif diff < small_diff:
+#             small_diff = diff
+#             small_pair = [array1[x], array2[y]]
 
-        if array1[x] < array2[y]:
-            y -= 1
+#         if array1[x] < array2[y]:
+#             y -= 1
+#         else:
+#             x -= 1
+
+#     return small_pair
+
+def smallestDifference(arrayOne, arrayTwo):
+    arrayOne.sort()
+    arrayTwo.sort()
+    idxOne = 0
+    idxTwo = 0
+
+    smallest = float("inf")
+    current = float("inf")
+    smallestPair = []
+
+    while idxOne < len(arrayOne) and idxTwo < len(arrayTwo):
+        firstNum = arrayOne[idxOne]
+        secondNum = arrayTwo[idxTwo]
+
+        if firstNum < secondNum:
+            current = secondNum - firstNum
+            idxOne += 1
+        elif secondNum < firstNum:
+            current = firstNum - secondNum
+            idxTwo += 1
         else:
-            x -= 1
+            return [firstNum, secondNum]
+        
+        if smallest > current:
+            smallest = current
+            smallestPair = [firstNum, secondNum]
 
-    return small_pair
+    return smallestPair
 
 
         
