5deb017206ea552d8298f99cd2d8dfb597d9ae83
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-04-24 17:01:56 -0600

    recursive method

--- spiral_traverse.py
+++ spiral_traverse.py
@@ -20,3 +20,22 @@
 #     return result
 
 def spiralTraverse(array):
+    result = []
+    spiralFill(array, 0, len(array) - 1, 0, len(array[0]) - 1, result)
+    return result
+
+def spiralFill(array, startRow, endRow, startCol, endCol, result):
+    if startRow > endRow or startCol > endCol:
+        return
+    for col in range(startCol, endCol + 1):
+        result.append(array[startRow][col])
+    for row in range(startRow + 1, endRow + 1):
+        result.append(array[row][endCol])
+    if startRow != endRow:
+        for col in reversed(range(startCol, endCol)):
+            result.append(array[endRow][col])
+    if startCol != endCol:
+        for row in reversed(range(startRow + 1, endRow)):
+            result.append(array[row][startCol])
+    
+    spiralFill(array, startRow + 1, endRow - 1, startCol + 1, endCol - 1, result)
