543b7c973b5f3b09af26f1b26227f9db79c8531a
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-04-24 11:25:23 -0600

    final mETE

--- move_element_to_end.py
+++ move_element_to_end.py
@@ -13,15 +13,26 @@
         
 #     return array
 
+# def moveElementToEnd(array, toMove):
+#     i = 0
+#     j = len(array) - 1
+
+#     while i < j:
+#         while i < j and array[j] == toMove:
+#             j -= 1
+#         if array[i] == toMove:
+#             array[i], array[j] = array[j], array[i]
+#         i += 1
+
+#     return array
+
 def moveElementToEnd(array, toMove):
-    i = 0
-    j = len(array) - 1
-
-    while i < j:
-        while i < j and array[j] == toMove:
-            j -= 1
-        if array[i] == toMove:
-            array[i], array[j] = array[j], array[i]
-        i += 1
-
-    return array
+    a = []
+    b = []
+    for i in array:
+        if i != toMove:
+            a.append(i)
+        else:
+            b.append(i)
+    c = a+b
+    return c
