b2df7b40c08b923e335499a7908f53ac2d6e28ab
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-04-24 11:22:26 -0600

    additional solution to move element

--- move_element_to_end.py
+++ move_element_to_end.py
@@ -1,15 +1,27 @@
-def moveElementToEnd(array, toMove):
-    idx = 0
-    totalIdx = 0
+# def moveElementToEnd(array, toMove):
+#     idx = 0
+#     totalIdx = 0
 
-    while totalIdx < len(array):
-        moving = None
-        if array[idx] == toMove:
-            moving = array.pop(idx)
-            array.append(moving)
-            idx -= 1
-        totalIdx += 1
-        idx += 1
+#     while totalIdx < len(array):
+#         moving = None
+#         if array[idx] == toMove:
+#             moving = array.pop(idx)
+#             array.append(moving)
+#             idx -= 1
+#         totalIdx += 1
+#         idx += 1
         
-    return array
+#     return array
 
+def moveElementToEnd(array, toMove):
+    i = 0
+    j = len(array) - 1
+
+    while i < j:
+        while i < j and array[j] == toMove:
+            j -= 1
+        if array[i] == toMove:
+            array[i], array[j] = array[j], array[i]
+        i += 1
+
+    return array
