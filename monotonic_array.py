7fe4f612059ceab2f38822287495e9f95824ef85
Ryan Pierce <pierce.ryan.97@gmail.com>
2023-04-24 15:56:48 -0600

    most readable Monotonic

--- monotonic_array.py
+++ monotonic_array.py
@@ -28,22 +28,33 @@
 
 #     return monotonic
 
-def isMonotonic(array):
-    if len(array) <= 2:
-        return True
+# def isMonotonic(array):
+#     if len(array) <= 2:
+#         return True
     
-    direction = array[1] - array[0]
-    for i in range(2, len(array)):
-        if direction == 0:
-            direction = array[i] - array[i-1]
-            continue
-        if breaksDirection(direction, array[i-1], array[i]):
-            return False
+#     direction = array[1] - array[0]
+#     for i in range(2, len(array)):
+#         if direction == 0:
+#             direction = array[i] - array[i-1]
+#             continue
+#         if breaksDirection(direction, array[i-1], array[i]):
+#             return False
         
-    return True
+#     return True
 
-def breaksDirection(direction, previousNum, currentNum):
-    difference = currentNum - previousNum
-    if direction > 0:
-        return difference < 0
-    return difference > 0
+# def breaksDirection(direction, previousNum, currentNum):
+#     difference = currentNum - previousNum
+#     if direction > 0:
+#         return difference < 0
+#     return difference > 0
+
+def isMonotonic(array):
+    isNonDecreasing = True
+    isNonIncreasing = True
+    for i in range(1, len(array)):
+        if array[i] < array[i-1]:
+            isNonDecreasing = False
+        if array[i] > array[i-1]:
+            isNonIncreasing = False
+
+    return isNonDecreasing or isNonIncreasing
